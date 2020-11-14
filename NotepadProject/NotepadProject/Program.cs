47a76ef5ff64a02cc8f25c59e40d8ce91d179c3e
Anna <anna.h.hallberg.92@gmail.com>
2020-11-14 11:09:29 +0100

    Give form a name and put it in the center

--- NotepadProject/NotepadProject/Program.cs
+++ NotepadProject/NotepadProject/Program.cs
@@ -16,7 +16,7 @@ namespace NotepadProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new NotepadForm());
         }
     }
 }
