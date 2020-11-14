1c74f2705df83c443b2420f300c2a44f30e3beb8
Anna <anna.h.hallberg.92@gmail.com>
2020-11-14 11:10:10 +0100

    Init

--- NotepadProject/NotepadProject/Form1.Designer.cs
+++ NotepadProject/NotepadProject/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace NotepadProject
 {
-    partial class Form1
+    partial class NotepadForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,18 @@ namespace NotepadProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // NotepadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "NotepadForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Notepad";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- NotepadProject/NotepadProject/Form1.cs
+++ NotepadProject/NotepadProject/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace NotepadProject
 {
-    public partial class Form1 : Form
+    public partial class NotepadForm : Form
     {
-        public Form1()
+        public NotepadForm()
         {
             InitializeComponent();
         }
