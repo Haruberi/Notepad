a73410b1c520c106d5327193e83a1c4756949240
Anna <anna.h.hallberg.92@gmail.com>
2020-11-14 11:20:28 +0100

    Add menu bar (in Swedish)

--- NotepadProject/NotepadProject/Form1.Designer.cs
+++ NotepadProject/NotepadProject/Form1.Designer.cs
@@ -29,21 +29,235 @@ namespace NotepadProject
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hjälpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nyttFönsterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.öppnaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sparaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sparaSomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ångraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kopieraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.klistraInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.taBortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teckenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomaInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomaUtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.återställStandardzoomingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visaHjälpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.omAnteckningarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.searchToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.hjälpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(433, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nyToolStripMenuItem,
+            this.nyttFönsterToolStripMenuItem,
+            this.öppnaToolStripMenuItem,
+            this.sparaToolStripMenuItem,
+            this.sparaSomToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.fileToolStripMenuItem.Text = "Arkiv";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ångraToolStripMenuItem,
+            this.kopieraToolStripMenuItem,
+            this.klistraInToolStripMenuItem,
+            this.taBortToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.editToolStripMenuItem.Text = "Redigera";
+            // 
+            // searchToolStripMenuItem
+            // 
+            this.searchToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.teckenToolStripMenuItem});
+            this.searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            this.searchToolStripMenuItem.Size = new System.Drawing.Size(70, 24);
+            this.searchToolStripMenuItem.Text = "Format";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoomaToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(50, 24);
+            this.viewToolStripMenuItem.Text = "Visa";
+            // 
+            // hjälpToolStripMenuItem
+            // 
+            this.hjälpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.visaHjälpToolStripMenuItem,
+            this.omAnteckningarToolStripMenuItem});
+            this.hjälpToolStripMenuItem.Name = "hjälpToolStripMenuItem";
+            this.hjälpToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.hjälpToolStripMenuItem.Text = "Hjälp";
+            // 
+            // nyToolStripMenuItem
+            // 
+            this.nyToolStripMenuItem.Name = "nyToolStripMenuItem";
+            this.nyToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.nyToolStripMenuItem.Text = "Ny";
+            // 
+            // nyttFönsterToolStripMenuItem
+            // 
+            this.nyttFönsterToolStripMenuItem.Name = "nyttFönsterToolStripMenuItem";
+            this.nyttFönsterToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.nyttFönsterToolStripMenuItem.Text = "Nytt fönster";
+            // 
+            // öppnaToolStripMenuItem
+            // 
+            this.öppnaToolStripMenuItem.Name = "öppnaToolStripMenuItem";
+            this.öppnaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.öppnaToolStripMenuItem.Text = "Öppna...";
+            // 
+            // sparaToolStripMenuItem
+            // 
+            this.sparaToolStripMenuItem.Name = "sparaToolStripMenuItem";
+            this.sparaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sparaToolStripMenuItem.Text = "Spara";
+            // 
+            // sparaSomToolStripMenuItem
+            // 
+            this.sparaSomToolStripMenuItem.Name = "sparaSomToolStripMenuItem";
+            this.sparaSomToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sparaSomToolStripMenuItem.Text = "Spara som...";
+            // 
+            // ångraToolStripMenuItem
+            // 
+            this.ångraToolStripMenuItem.Name = "ångraToolStripMenuItem";
+            this.ångraToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ångraToolStripMenuItem.Text = "Klipp ut";
+            // 
+            // kopieraToolStripMenuItem
+            // 
+            this.kopieraToolStripMenuItem.Name = "kopieraToolStripMenuItem";
+            this.kopieraToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.kopieraToolStripMenuItem.Text = "Kopiera";
+            // 
+            // klistraInToolStripMenuItem
+            // 
+            this.klistraInToolStripMenuItem.Name = "klistraInToolStripMenuItem";
+            this.klistraInToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.klistraInToolStripMenuItem.Text = "Klistra in";
+            // 
+            // taBortToolStripMenuItem
+            // 
+            this.taBortToolStripMenuItem.Name = "taBortToolStripMenuItem";
+            this.taBortToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.taBortToolStripMenuItem.Text = "Ta bort";
+            // 
+            // teckenToolStripMenuItem
+            // 
+            this.teckenToolStripMenuItem.Name = "teckenToolStripMenuItem";
+            this.teckenToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.teckenToolStripMenuItem.Text = "Tecken...";
+            // 
+            // zoomaToolStripMenuItem
+            // 
+            this.zoomaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoomaInToolStripMenuItem,
+            this.zoomaUtToolStripMenuItem,
+            this.återställStandardzoomingToolStripMenuItem});
+            this.zoomaToolStripMenuItem.Name = "zoomaToolStripMenuItem";
+            this.zoomaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.zoomaToolStripMenuItem.Text = "Zooma";
+            // 
+            // zoomaInToolStripMenuItem
+            // 
+            this.zoomaInToolStripMenuItem.Name = "zoomaInToolStripMenuItem";
+            this.zoomaInToolStripMenuItem.Size = new System.Drawing.Size(268, 26);
+            this.zoomaInToolStripMenuItem.Text = "Zooma in";
+            // 
+            // zoomaUtToolStripMenuItem
+            // 
+            this.zoomaUtToolStripMenuItem.Name = "zoomaUtToolStripMenuItem";
+            this.zoomaUtToolStripMenuItem.Size = new System.Drawing.Size(268, 26);
+            this.zoomaUtToolStripMenuItem.Text = "Zooma ut";
+            // 
+            // återställStandardzoomingToolStripMenuItem
+            // 
+            this.återställStandardzoomingToolStripMenuItem.Name = "återställStandardzoomingToolStripMenuItem";
+            this.återställStandardzoomingToolStripMenuItem.Size = new System.Drawing.Size(268, 26);
+            this.återställStandardzoomingToolStripMenuItem.Text = "Återställ standardzooming";
+            // 
+            // visaHjälpToolStripMenuItem
+            // 
+            this.visaHjälpToolStripMenuItem.Name = "visaHjälpToolStripMenuItem";
+            this.visaHjälpToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.visaHjälpToolStripMenuItem.Text = "Visa hjälp";
+            // 
+            // omAnteckningarToolStripMenuItem
+            // 
+            this.omAnteckningarToolStripMenuItem.Name = "omAnteckningarToolStripMenuItem";
+            this.omAnteckningarToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.omAnteckningarToolStripMenuItem.Text = "Om anteckningar";
             // 
             // NotepadForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(433, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "NotepadForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Notepad";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nyttFönsterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem öppnaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sparaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sparaSomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ångraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kopieraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem klistraInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem taBortToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teckenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomaInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomaUtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem återställStandardzoomingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hjälpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visaHjälpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem omAnteckningarToolStripMenuItem;
     }
 }
 
